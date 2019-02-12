a3499053c444a09c3525540fef31e58c00aea558
allenyhu <allenyhu@usc.edu>
2019-02-12 23:56:19 +0000

    ds grpc client

--- src/store/data_store_client.cc
+++ src/store/data_store_client.cc
@@ -1,14 +1,35 @@
 #include <memory>
 #include <string>
+#include <thread>
+#include <vector>
 
+#include <grpc/grpc.h>
+#include <grpcpp/channel.h>
+#include <grpcpp/client_context.h>
+#include <grpcpp/create_channel.h>
 #include <grpcpp/grpcpp.h>
+#include <grpcpp/security/credentials.h>
 
+#include "data_store.grpc.pb.h"
+
+using chirp::DeleteRequest;
+using chirp::DeleteReply;
+using chirp::GetRequest;
+using chirp::GetReply;
+using chirp::KeyValueStore;
+using chirp::PutRequest;
+using chirp::PutReply;
 using grpc::Channel;
 using grpc::ClientContext;
+using grpc::ClientReaderWriter;
 using grpc::Status;
-using chrip::PutRequest;
-using chirp::PutReply;
-using chirp::KeyValueStore;
+
+// Helper method to make a GetRequest
+GetRequest MakeGetRequest(const std::string& key) {
+  GetRequest r;
+  r.set_key(key);
+  return r;
+}
 
 // grpc client to communicate with DataStoreServer
 class DataStoreClient {
@@ -28,31 +49,40 @@ class DataStoreClient {
     PutReply reply;
     ClientContext context;
 
-    Status status = stub_->Put(&context, request, &reply);
-
-    if(status.ok()) {
-      return reply.message();
-    } else {
-      return false;
-    }
+    Status status = stub_->put(&context, request, &reply);
+    return status.ok();
   }
 
   // Get command to send GetRequest and receive GetReply
   // @key: key to be retrieved from
   // @ret: value retrieved from DataStore
-  std::string Get(const std::string& key) {
-    GetRequest request;
-    request.set_key(key);
+  std::vector<std::string> Get(const std::vector<std::string>& keys) {
+    ClientContext context;
+    
+    std::shared_ptr<ClientReaderWriter<GetRequest, GetReply> > stream(
+      stub_->get(&context));
+    
+    std::thread writer([stream, keys]() {
+      for(const std::string& s : keys) {
+        std::cout << "GetRequest: " << s << std::endl;
+        stream->Write(MakeGetRequest(s));
+      }
+      stream->WritesDone();
+    });
 
     GetReply reply;
-    ClientContext context;
-
-    Status status = stub_->Get(&context, request, &reply);
+    std::vector<std::string> replies;
+    while(stream->Read(&reply)) {
+      std::cout << "GetReply: " << reply.value() << std::endl;
+      replies.push_back(reply.value());
+    }
+    writer.join();
+    Status status = stream->Finish();
 
     if(status.ok()) {
-      return reply.message();
+      return replies;
     } else {
-      return "";
+      return std::vector<std::string>{};
     }
   }
 
@@ -60,23 +90,26 @@ class DataStoreClient {
   // @key: key to be deleted
   // @ret: ture or false based on success
   bool DeleteKey(const std::string& key) {
-    GetRequest request;
+    DeleteRequest request;
     request.set_key(key);
 
-    GetReply reply;
+    DeleteReply reply;
     ClientContext context;
 
-    Status status = stub_->DeleteKey(&context, request, &reply);
-
-    if(status.ok()) {
-      return reply.message();
-    } else {
-      return false;
-    }
+    Status status = stub_->deletekey(&context, request, &reply);
+    return status.ok();
   }
 
  private:
   std::unique_ptr<KeyValueStore::Stub> stub_;
 };
 
-// TODO do we need to make a main like in the grpc examples?
+int main(int argc, char** argv) {
+ DataStoreClient client(grpc::CreateChannel("localhost:50000",
+			grpc::InsecureChannelCredentials()));
+ std::cout << client.Put("test", "test text") << std::endl;
+ std::vector<std::string> v {"test"};
+ std::cout << client.Get(v)[0] << std::endl;
+
+ return 0; 
+}
